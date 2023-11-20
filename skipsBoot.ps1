db341626eff544b374ba9a431d72f2adfe3ba199
kapten Angkasa <alvinsatrianugraha@yahoo.co.id>
2023-11-20 22:20:19 +0700

    Update skipsBoot.ps1

--- skipsBoot.ps1
+++ skipsBoot.ps1
@@ -1,5 +1,8 @@
-$registryPath = "HKLM:\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\OOBE"
-
-New-ItemProperty -Path $registryPath -Name "SkipMachineOOBE" -Value 1 -PropertyType DWORD -Force
-New-ItemProperty -Path $registryPath -Name "SkipUserOOBE" -Value 1 -PropertyType DWORD -Force
-New-ItemProperty -Path $registryPath -Name "ProtectYourPC" -Value 3 -PropertyType DWORD -Force
+Write-Host "shell privacy handle.."
+New-ItemProperty -Path "HKLM:\HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\OOBE" -Name "PrivacyConsentStatus" -Value 1 -PropertyType DWORD -Force
+Write-Host "shell skip machine oobe.."
+New-ItemProperty -Path "HKLM:\HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\OOBE" -Name "SkipMachineOOBE" -Value 1 -PropertyType DWORD -Force
+Write-Host "shell protect your PC.."
+New-ItemProperty -Path "HKLM:\HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\OOBE" -Name "ProtectYourPC" -Value 3 -PropertyType DWORD -Force
+Write-Host "shell skip user OOBE.."
+New-ItemProperty -Path "HKLM:\HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\OOBE" -Name "SkipUserOOBE" -Value 1 -PropertyType DWORD -Force
